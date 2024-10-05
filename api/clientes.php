60a0ca07593250b5f98f65986b7f89c0473d233e
obedeDintala123 <obededintala@gmail.com>
2024-10-05 21:20:03 +0100

    Update Clientes

--- api/clientes.php
+++ api/clientes.php
@@ -7,14 +7,27 @@ header('Content-Type: application/json');
 include 'index.php';
 
 try {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-    if($_SERVER['REQUEST_METHOD'] === "POST"){
-        $sql = "SELECT * FROM clientes";
-        $stmt = $conexao -> query($sql);
-        $clientes = $stmt -> fetchAll(PDO::FETCH_ASSOC);
-        echo json_encode($clientes);
+        $email = trim(htmlspecialchars($_POST['email']));
+        $password = trim(htmlspecialchars($_POST['password']));
+
+        if (!empty($email) || !empty($password)) {
+            $sql = 'SELECT * FROM clientes WHERE email = :codeOne';
+            $stmt = $conexao->prepare($sql);
+            $stmt->bindParam(":codeOne", $email);
+            $stmt->execute();
+            $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if ($user && password_verify($password, $user["password"])) {
+                echo json_encode(["success" => true]);
+            } else {
+                echo json_encode(["success" => false, "message" => "Email ou password incorrect"]);
+            }
+        } else {
+            echo json_encode(["success" => false, "message" => "Email ou password não foram preenchidos"]);
+        }
     }
-
 } catch (PDOException $e) {
     echo json_encode(["error" => $e->getMessage()]);
 }
